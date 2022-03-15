a8a48a9471f73b3dde75d8d4c7746123ed4d1716
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2022-03-15 21:31:20 +0400

    ssc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // template:`<h1> {{title}} </h1>`,
+  // styles:['h1{color:green; text-transform:uppercase}']
+  // encapsulation:ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 251;
+  title = 54;
 
 
   myval(ev:any){
- 
     this.title = ev
   }
 
